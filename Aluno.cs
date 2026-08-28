9b376974a95df47c5347ad86b5ce4621a5efb163
thayssaalexandre <thayssa.alexandre@edu.senai.br>
2026-08-28 09:48:56 -0300

    .

---
+++ Aluno.cs
@@ -0,0 +1,19 @@
+﻿namespace Abstracao
+{
+    public class Aluno
+    {
+        public string Nome;
+        public int Matricula;
+        public double Nota1;
+        public double Nota2;
+
+        public void CalcularMedia()
+        {
+           double media = (Nota1 + Nota2)/2;
+           Console.WriteLine($"Aluna(o): {Nome}\nNúmero da matrícula: {Matricula}\nNotas: {Nota1} || {Nota2}");
+           Console.WriteLine($"Média do aluno: {media}");
+        }
+
+    }
+       
+}
