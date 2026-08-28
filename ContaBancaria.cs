58e410bd789f50da8e0aea253e65673b90905099
thayssaalexandre <thayssa.alexandre@edu.senai.br>
2026-08-28 11:23:33 -0300

    Atividade 3

---
+++ ContaBancaria.cs
@@ -0,0 +1,32 @@
+﻿namespace Abstracao
+{
+    public class ContaBancaria
+    {
+        public string Nome;
+        public int NumeroConta;
+        public double Saldo;
+        public double Deposito;
+        public double Retirada;
+        public void ExibirInformacoes()
+        {
+            double TotalDeposito = Saldo + Deposito;
+            double TotalRetirada = TotalDeposito - Retirada;
+            if (TotalDeposito >= Retirada)
+            {
+                Console.WriteLine($"Nome do Titular: {Nome}");
+                Console.WriteLine($"Número da Conta: {NumeroConta}, Saldo: {Saldo}");
+                Console.WriteLine($"Deposito:R${Deposito}, Saldo atualizado: {TotalDeposito}");
+                Console.WriteLine($"Retirada: R${Retirada}, Retirada com o saldo atualizado: {TotalRetirada}");
+                Console.WriteLine($"------------------------");
+            }
+            else
+            {
+                Console.WriteLine($"Nome: {Nome}");
+                Console.WriteLine($"Número da Conta: {NumeroConta}, Saldo: {Saldo}");
+                Console.WriteLine($"Deposito:R${Deposito}, Saldo atualizado: {TotalDeposito}");
+                Console.WriteLine($"Retirada: R${Retirada}, Retirada com o saldo atualizado: Não Foi Possível a Retirida do valor solicitado");
+                Console.WriteLine($"------------------------");
+            }
+        }
+    }
+}
