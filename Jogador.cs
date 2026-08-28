132918913075c70b1e1a42ce0a72a3511e8c42d5
thayssaalexandre <thayssa.alexandre@edu.senai.br>
2026-08-28 13:42:12 -0300

    Ultimo commit

--- Jogador.cs
+++ Jogador.cs
@@ -1,10 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Abstracao
+﻿namespace Abstracao
 {
-    internal class Jogador
+    public class Jogador
     {
+        public string Nome;
+        public string Posicao;
+        public int NumeroCamisa;
+
     }
 }
---
+++ Time.cs
@@ -0,0 +1,23 @@
+﻿namespace Abstracao
+{
+    public class Time
+    {
+        public string NomeDoTime;
+        public List<Jogador> ListaDeJogadores = [];
+
+        public void AdicionarJogador(Jogador jogador)
+        {
+            ListaDeJogadores.Add(jogador);
+            Console.WriteLine($"O jogador {jogador.Nome} foi adicionado ao time {NomeDoTime}.");
+        }
+
+        public void ListarJogadores()
+        {
+            Console.WriteLine($"Escalação do time {NomeDoTime}:");
+            foreach (var jogador in ListaDeJogadores)
+            {
+                Console.WriteLine($"Nome: {jogador.Nome}, Posição: {jogador.Posicao}, Número da Camisa: {jogador.NumeroCamisa}");
+            }
+        }
+    }
+}
